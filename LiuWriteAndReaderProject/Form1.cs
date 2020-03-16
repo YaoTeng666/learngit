01547393b09154747f895329cad3bd8f54113da7
yaoteng44@gmail.com <yaoteng44@gmail.com>
2020-03-16 15:24:53 +0800

    LiuProject

---
+++ LiuWriteAndReaderProject/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LiuWriteAndReaderProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){Age=21,Name="黑白红",Sex="女"},
+                new Student(){Age=22,Name="白黑",Sex="男"},
+                new Student(){Age=23,Name="白红",Sex="女"},
+                new Student(){Age=24,Name="净白",Sex="男"}
+            };
+
+            StreamWriter sw = new StreamWriter("StuInfo.dat");
+
+
+            foreach (Student item in students)
+            {
+                sw.WriteLine(item.ToString());
+            }
+            sw.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader sr =new StreamReader("StuInfo.dat");
+            //新建一个集合存储对象
+            List<Student> NewStudents = new List<Student>();
+            //声明一个字符串以获取文件中读取的字符串
+            string StrStudent = null;
+
+            do
+            {
+                StrStudent = sr.ReadLine();
+                if (StrStudent!=null)
+                {
+                    NewStudents.Add(Student.ReturnStudent(StrStudent));
+                }
+
+
+            } while (StrStudent!=null);
+
+            listBox1.DisplayMember = "Name";
+            listBox1.DataSource = NewStudents;
+
+            sr.Close();
+
+        }
+    }
+}
---
+++ LiuWriteAndReaderProject/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiuWriteAndReaderProject
+{
+    class Student
+    {
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        public int Age { get; set; }
+
+        //重写tostring（）方法，指定格式字符串
+        public override string ToString()
+        {
+            return string.Format("Name:{0},Sex:{1},Age:{2}", Name, Sex, Age);
+        }
+
+
+        /// <summary>
+        /// 将字符串转换Student对象的方法
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static Student ReturnStudent(string str)
+        {
+            //创建一个Student实例
+            Student s = new Student();
+            //将字符串以‘，’字符切割字符串
+            string[] strs = str.Split(',');
+            //处理数组中第一个元素以获取Name属性
+            s.Name = strs[0].Split(':')[1];
+            //处理数组中第二个元素以获取Sex属性
+            s.Sex = strs[1].Split(':')[1];
+            //处理数组中第三个元素以获取Age属性
+            s.Age = int.Parse(strs[2].Split(':')[1]);
+            //返回转换完成的对象
+            return s;
+        }
+    }
+}
